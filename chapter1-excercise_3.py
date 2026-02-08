b70901f4d30564901d2acce8df7298734cfc4dee
beggyoshi <vuduchuyen2007@gmail.com>
2026-02-08 13:22:02 +0700

    Remove all exercise files from branch

--- chapter1-excercise_3.py
+++
@@ -1,5 +0,0 @@
-safety_limit_torque= 5.0
-torques= [1.2, 5.5, 0.8, 10.2, 4.9, -2.1, 7.0]
-safe_torques=[t for t in torques if abs(t)<=5.0 ]
-safe_torques.append(0.0)
-print(f"validated commands: {safe_torques}")
--- chapter1-excercise_4.py
+++
@@ -1,6 +0,0 @@
-raw_gps=(10.823, 106.629, 5.0)
-lat , lon, _ = raw_gps
-def get_velocity():
-    return(3.0,-2)
-vx,vy=get_velocity()
-print(f"Velocity: x ={vx}m/s, velocity: y={vy}m/s" )
